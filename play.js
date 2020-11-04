85ec731771202e64b4829720c92ca179a64cef7d
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-04 21:07:38 +0000

    Play Sanake initial

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  conn.on('data', () => {
+    console.log("you ded cuz you idled");
+  });
+  
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+
+console.log('Connecting ...');
+connect();
